6724ae23e6040bf589fb7d034b10e345bc6a74f0
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2025-02-24 01:08:45 +0600

    issufix

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -31,39 +31,40 @@ export const getQuestions = async (params: GetQuestionsParams) => {
 export const createQuestion = async (params: CreateQuestionParams) => {
   try {
     // connect to DB
-    connectToDatabase();
+    await connectToDatabase();
 
     const { title, content, tags, author, path } = params;
 
     // Create the Question
-    const question = await Question.create({
-      title,
-      content,
-      author,
-    });
+    const question = await Question.create({ title, content, author });
 
     const tagDocuments = [];
 
     // Create the tags or get them if they already exist
     for (const tag of tags) {
       const existingTag = await Tag.findOneAndUpdate(
-        {
-          name: { $regex: new RegExp(`^${tag}$`, "i") },
-        },
-        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag } },
         { upsert: true, new: true }
       );
+
+      if (!existingTag.question.includes(question._id)) {
+        existingTag.question.push(question._id);
+        await existingTag.save();
+      }
 
       tagDocuments.push(existingTag._id);
     }
 
-    await Question.findOneAndUpdate(question._id, {
+    await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocuments } },
     });
 
-    // Create an interaction record for the user's asked questions action
+    await User.findByIdAndUpdate(author, { $inc: { reputation: 5 } });
 
-    // Increment author's reputation by +5 for creating a question
     revalidatePath(path);
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error creating question:", error);
+    throw error;
+  }
 };
